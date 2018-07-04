398218d122d8534b7a79b051ad2d24be0dc7f044
sampathkumar <sampath1983jan@gmail.com>
2018-07-04 18:36:05 +0530

    this is new version

---
+++ TechSharpy.Entitifer/Core/EnityInstanceLookUp.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using TechSharpy.Entitifier.Data;
+namespace TechSharpy.Entitifier
+{
+    public enum LookUpType {
+        _None=0,
+        _Year=1,
+        _Month=2,
+        _Gender=3,
+        _Nationality=4,
+        _Currency=5,
+        _Quarter=6,
+        _YesNo=7,
+        _Relationship=8,
+        _Color=9,
+        _BoodGroup=10,
+        _Country=11,
+    }
+    
+   public class EnityInstanceLookUp
+    {
+        public int LookUpID;
+        public string Name;
+        public bool IsCore;
+        public bool HaveChild;
+        public List<LookUpItem> LookUpItems;
+        public LookUpType LookUpType;
+        private Entitifier.Data.LookUp dataLookup; 
+        public EnityInstanceLookUp(int lookUpID, string name, bool isCore, bool haveChild, List<LookUpItem> lookUpItems, LookUpType lookUpType)
+        {
+            LookUpID = lookUpID;
+            Name = name;
+            IsCore = isCore;
+            HaveChild = haveChild;
+            LookUpItems = lookUpItems ?? throw new ArgumentNullException(nameof(lookUpItems));
+            LookUpType = lookUpType;
+            dataLookup = new LookUp();
+        }
+
+        public EnityInstanceLookUp(int lookUpID)
+        {
+            LookUpID = lookUpID;
+            Name = "";
+            IsCore = false;
+            HaveChild = false;
+            LookUpItems = new List<LookUpItem>();
+            LookUpType = LookUpType._None;
+            dataLookup = new LookUp();
+        }
+
+        public void Init() {
+            DataTable dt, dtLookUp = new DataTable();
+            dt = dataLookup.GetLookUpItems(-1, LookUpID);
+            dtLookUp = dt.DefaultView.ToTable(true, "ClientID", "LookUpId", "LookUPName", "IsCore", "HaveChild",  "LookUpType");
+            foreach (DataRow dr in dtLookUp.Rows)
+            {
+                Name = dr["LookUPName"] == DBNull.Value ? "" : dr["LookUPName"].ToString();
+                IsCore = dr["IsCore"] == DBNull.Value ? false : Convert.ToBoolean(dr["IsCore"]);
+                LookUpType = dr["LookUpType"] == DBNull.Value ?  LookUpType._None :  (LookUpType)dr["LookUpType"];
+                HaveChild = dr["HaveChild"] == DBNull.Value ? false : Convert.ToBoolean(dr["HaveChild"]);
+            }
+            foreach (DataRow dr in dt.Rows)
+            {
+                LookUpItem item = new LookUpItem{                   
+                    ItemID = dr["LookupInstanceID"] == DBNull.Value ? -1 : Convert.ToInt32(dr["LookupInstanceID"]),
+                    LookUpID = this.LookUpID,
+                    Name = dr["ItemName"] == DBNull.Value ? "" : Convert.ToString(dr["ItemName"]),
+                    ShortName = dr["ShortName"] == DBNull.Value ? "" : Convert.ToString(dr["ShortName"]),
+                    Order = dr["Order"] == DBNull.Value ? -1 : Convert.ToInt32(dr["Order"]),
+                    ParentLookUpID = dr["ParentLookUpID"] == DBNull.Value ? -1 : Convert.ToInt32(dr["ParentLookUpID"]),
+                };
+                LookUpItems.Add(item);
+            }
+        }
+        public int AddLookUpItem(int lookupID, LookUpItem lookUpItem) {
+            int inext;
+            if (lookUpItem.ItemID > 0)
+            {
+                if (dataLookup.SaveItem(-1, lookUpItem.ItemID, lookupID, lookUpItem.Name, lookUpItem.ShortName, lookUpItem.Order, lookUpItem.ParentLookUpID))
+                {
+                    inext = lookUpItem.ItemID;
+                }
+                else {
+                    inext = -1;
+                };
+            }
+            else {
+                inext = dataLookup.SaveItem(-1, lookupID, lookUpItem.Name, lookUpItem.ShortName, lookUpItem.Order, lookUpItem.ParentLookUpID);
+            }           
+            return inext;
+        }
+        public bool RemoveLookUpItem(int itemID) {
+            if (dataLookup.DeleteLookUpItem(-1, LookUpID, itemID))
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        public bool Save() {
+            int inext;
+            if (this.LookUpID > 0)
+            {
+                dataLookup.Save(-1, this.LookUpID, this.Name, this.IsCore, this.HaveChild, this.LookUpType);
+                foreach (LookUpItem item in this.LookUpItems)
+                {
+                    AddLookUpItem(this.LookUpID, item);
+                }
+            }
+            else {
+                inext = dataLookup.Save(-1, this.Name, this.IsCore, this.HaveChild, this.LookUpType);
+                if (inext > 0)
+                {
+                    foreach (LookUpItem item in this.LookUpItems)
+                    {
+                        AddLookUpItem(inext, item);
+                    }
+                }
+            }           
+            return true;
+        }
+
+        public bool Remove() {
+            if (dataLookup.Delete(-1, LookUpID)) {
+                dataLookup.DeleteLookUpItems(-1, LookUpID);
+            }
+            return true;
+        }                      
+    }
+
+   public class LookUpItem {
+        public int ItemID;
+        public int LookUpID;
+        public string Name;
+        public string ShortName;
+        public int Order;
+        public int ParentLookUpID;
+        public LookUpItem() {
+
+        }
+        public LookUpItem(int itemID, int lookUpID, string value, string shortName, int order, int parentLookUpID)
+        {
+            ItemID = itemID;
+            LookUpID = lookUpID;
+            Name = value ?? throw new ArgumentNullException(nameof(value));
+            ShortName = shortName ?? throw new ArgumentNullException(nameof(shortName));
+            Order = order;
+            ParentLookUpID = parentLookUpID;
+        }
+
+        public LookUpItem(int lookUpID)
+        {
+            LookUpID = lookUpID;
+            ItemID = -1;
+            Name = "";
+            ShortName = "";
+            Order = 0;
+            ParentLookUpID = -1;
+        }
+
+    }
+
+}
---
+++ TechSharpy.Entitifer/Core/EntityInstance.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechSharpy.Entitifier
+{
+    public class EntityField
+    {
+        public string Name;
+        public Int32 InstanceID;
+        public EntityFieldType FieldType;
+        public bool IsKey;
+        public bool IsRequired;
+        public bool IsUnique;
+        public Int32 LookUpID;
+        public bool IsCore;
+        public bool EnableEncription;
+        public Int32 EntityKey;
+        public string Value;
+        public bool IsReadOnly;
+        public string DefaultValue;
+        public int DisplayOrder;
+        public List<string> LookUpArray;
+        public bool enableLimit;
+        public string Min;
+        public string Max;
+        public int MaxLength;
+        public string DisplayName; 
+        public bool AutoIncrement;
+        public Int64 Incrementfrom;
+        public Int64 Incrementby;
+        public bool IsShow;
+        public string Description;
+        private Data.EntitySchema dataEntity;
+        public EntityField() {
+            InstanceID = -1;
+            Name = "";
+            InstanceID = -1;
+            FieldType = EntityFieldType._Text;
+            IsKey = false;
+            IsRequired = false;
+            IsUnique = false;
+            LookUpID = -1;
+            IsCore = false;
+            EntityKey = -1;
+            Value = "";
+            IsReadOnly = false;
+            DefaultValue = "";
+            DisplayOrder = -1;
+            LookUpArray = new List<string>();
+            Min = "0";
+            Max = "0";
+            MaxLength = 0;
+            DisplayName = "";
+            AutoIncrement = false;
+            Incrementfrom = 0;
+            Incrementby = 0;
+            Description = "";
+            enableLimit = false;
+            EnableEncription = false;
+            dataEntity = new Data.EntitySchema();
+        }
+        public EntityField(string name, int instanceID, EntityFieldType fieldType, bool isKey, bool isRequired, bool isUnique, 
+            int lookUpID, bool isCore, int entityKey, string value, bool isReadOnly, string defaultValue, int displayOrder,
+            List<string> lookUpArray, string min, string max, int maxLength, string displayName, bool autoIncrement, long incrementfrom,
+            long incrementby,string description,bool enableencription,bool enablelimit)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            InstanceID = instanceID;
+            FieldType = fieldType;
+            IsKey = isKey;
+            IsRequired = isRequired;
+            IsUnique = isUnique;
+            LookUpID = lookUpID;
+            IsCore = isCore;
+            EntityKey = entityKey;
+            Value = value ?? throw new ArgumentNullException(nameof(value));
+            IsReadOnly = isReadOnly;
+            DefaultValue = defaultValue ?? throw new ArgumentNullException(nameof(defaultValue));
+            DisplayOrder = displayOrder;
+            LookUpArray = lookUpArray ?? throw new ArgumentNullException(nameof(lookUpArray));
+            Min = min ?? throw new ArgumentNullException(nameof(min));
+            Max = max ?? throw new ArgumentNullException(nameof(max));
+            MaxLength = maxLength;
+            DisplayName = displayName ?? throw new ArgumentNullException(nameof(displayName));
+            AutoIncrement = autoIncrement;
+            Incrementfrom = incrementfrom;
+            Incrementby = incrementby;
+            IsShow = true;
+            Description = description;
+            enableLimit = enablelimit;
+            EnableEncription = enableencription;
+            dataEntity = new Data.EntitySchema();
+        }
+
+        public EntityField(int pinstanceID)
+        {
+            InstanceID = pinstanceID;
+            Name = "";
+            InstanceID = -1;
+            FieldType = EntityFieldType._Text;
+            IsKey = false;
+            IsRequired = false;
+            IsUnique = false;
+            LookUpID = -1;
+            IsCore = false;
+            EntityKey = -1;
+            Value = "";
+            IsReadOnly = false;
+            DefaultValue = "";
+            DisplayOrder = -1;
+            LookUpArray =  new List<string>();
+            Min = "0";
+            Max = "0";
+            MaxLength = 0;
+            DisplayName = "";
+            AutoIncrement = false ;
+            Incrementfrom = 0;
+            Incrementby = 0;
+            EnableEncription = false;
+            Description = "";
+            enableLimit = false;
+            dataEntity = new Data.EntitySchema();
+        }
+
+        public bool Save() {
+             
+            if (this.InstanceID > 0)
+            {
+                return (dataEntity.SaveField(this.Name, this.InstanceID, this.FieldType, this.IsKey, this.IsRequired, this.IsUnique, this.LookUpID, this.IsCore, this.EntityKey, this.Value, this.IsReadOnly
+                , this.DefaultValue, this.DisplayOrder, new List<string>(), this.Min, this.Max, this.MaxLength, this.DisplayName, this.AutoIncrement, this.Incrementfrom,
+                this.Incrementby, this.Description, this.EnableEncription, this.enableLimit));                                        
+
+            }
+            else {
+                int next = 0;
+                next =  dataEntity.SaveField(this.Name, this.FieldType, this.IsKey, this.IsRequired, this.IsUnique, this.LookUpID, this.IsCore, this.EntityKey, this.Value, this.IsReadOnly
+                , this.DefaultValue, this.DisplayOrder, new List<string>(), this.Min, this.Max, this.MaxLength, this.DisplayName, this.AutoIncrement, this.Incrementfrom,
+                this.Incrementby, this.Description, this.EnableEncription, this.enableLimit);
+                this.InstanceID = next;
+                if (next > 0) {
+                    return true;
+                }               
+            }            
+            return false;
+        }
+
+        public bool Remove() {
+            return true;
+        }
+
+        public bool Hide() {
+            return true;
+        }
+
+    }
+}
