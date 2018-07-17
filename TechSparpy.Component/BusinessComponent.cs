47af6dec1cacbfc22058e417943db978d3440fcc
sampathkumar <sampath1983jan@gmail.com>
2018-07-17 18:10:11 +0530

    Component Modal added

--- TechSparpy.Component/BusinessComponent.cs
+++ TechSparpy.Component/BusinessComponent.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TechSparpy.Component
+namespace TechSharpy.Component
 {
-    public class Class1
+    class BusinessComponent
     {
+
     }
 }
--- TechSparpy.Component/SystemComponent.cs
+++ TechSparpy.Component/SystemComponent.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TechSharpy.Entitifier
+namespace TechSharpy.Component
 {
-    public class Class1
+    class SystemComponent
     {
     }
 }
