64e478bbb8a3d48282ebfbc77ce46ce7c2cab991
sampathkumar <sampath198jan@gmail.com>
2018-07-27 07:14:15 +0530

    model work started

--- TechSparpy.Component/Model/ComponentModel.cs
+++ TechSparpy.Component/Model/ComponentModel.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace TechSharpy.Component.Model
 {
-    class ComponentModel
+    public interface IComponentModel {
+        bool SaveModel();
+        bool RemoveModel();
+        bool RemoveElement();
+        void Load();
+
+    }
+
+    public class ComponentModel : Entitifier.Core.EntityModel, IComponentModel
+
     {
+
+        public List<ElementRelation> elementRelations;
+
+        public ComponentModel() : base() {
+
+        }
+        public void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveElement()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveModel()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ TechSparpy.Component/Model/ElementRelation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSharpy.Component.Model
+{
+    public class ElementRelation : Entitifier.Core.EntityNode
+    {
+        public List<ElementRelationNode> RelationNodes;
+
+        public ElementRelation(int modelID, int Componentkey, int leftIndex, int rightIndex, int relatedComponentkey) :
+            base(modelID, Componentkey, leftIndex, rightIndex, relatedComponentkey)
+        {
+            RelationNodes = new List<ElementRelationNode>();
+        }
+    }
+
+    public class ElementRelationNode : Entitifier.Core.NodeJoint {
+        public ElementRelationNode(string leftAttributeKey, string rightAttributeKey) : base(leftAttributeKey, rightAttributeKey) {
+
+        }
+    }
+}
