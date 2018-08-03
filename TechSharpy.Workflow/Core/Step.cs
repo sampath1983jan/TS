4f80a63c59055622177761ea3378f8850f806610
sampathkumar <sampath1983jan@gmail.com>
2018-08-03 18:23:23 +0530

    s

---
+++ TechSharpy.Workflow/Core/Step.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSharpy.Workflow.Core
+{
+    public enum StepType {
+        _initator=1,
+        _approver=2,
+        _processer=3
+    }
+    public class Step
+    {
+        
+    }
+}
---
+++ TechSharpy.Workflow/Core/Workflow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSharpy.Workflow.Core
+{
+  public class Workflow
+    {
+        public int ID;
+        public string Name;
+        public string Category;
+        public string Description;
+        public DateTime CreatedOn;
+        public int Createdby;
+
+    }
+}
